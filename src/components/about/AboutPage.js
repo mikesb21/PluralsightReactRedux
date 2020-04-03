c3a48131e64c1e107878153719f2d601cb21d4df
Michael <miker.sb7@gmail.com>
2020-04-03 17:09:34 -0600

    Added home, about and courses pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This app uses React, Redux and React router, and many other helpful
+      libraries.
+    </p>
+  </div>
+);
+
+export default HomePage;
